cb779fb0d3d0728f5e3fdd76d1839446bfb7f55c
Maiza Volpato dos Santos <maiza.volpato@gmail.com>
2025-12-29 15:11:02 +0000

    Adj the import of Header and Main and delete fonts.css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState, useContext } from "react";
-import Header from "./Header/Header";
-import Main from "./Main/Main";
+import Header from "./header/Header";
+import Main from "./main/Main";
 import Footer from "./footer/Footer";
-import ImagePopup from "./Main/components/imagePopup/ImagePopup";
+
 import { api } from "../utils/api";
 import CurrentUserContext from "../contexts/CurrentUserContext";
 
